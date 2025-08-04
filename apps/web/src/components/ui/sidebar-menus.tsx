9637dca3cc4ddd7d3565d423ca46d9cef0c40674
peal-26 <edilasio@live.com>
2025-08-04 21:04:07 +0100

    FIX: Sidebar vertical center

--- apps/web/src/components/ui/sidebar-menus.tsx
+++ apps/web/src/components/ui/sidebar-menus.tsx
@@ -12,19 +12,21 @@ export function SidebarMenus() {
   const pathname = usePathname();
 
   return (
-    <div className="flex h-full flex-col justify-center gap-2 overflow-y-auto">
-      {MENUS.map((menu, index) => (
-        <Link
-          key={index}
-          href={menu.href}
-          className={cn(
-            'rounded-md p-2 text-base font-medium text-white transition-all duration-300 hover:bg-white hover:text-primary',
-            activeMenu(menu.href, pathname) ? 'bg-white text-primary' : '',
-          )}
-        >
-          {menu.title}
-        </Link>
-      ))}
+    <div className="flex h-full overflow-y-auto">
+      <div className="my-auto flex w-full flex-col gap-2">
+        {MENUS.map((menu, index) => (
+          <Link
+            key={index}
+            href={menu.href}
+            className={cn(
+              'rounded-md p-2 text-base font-medium text-white transition-all duration-300 hover:bg-white hover:text-primary',
+              activeMenu(menu.href, pathname) ? 'bg-white text-primary' : '',
+            )}
+          >
+            {menu.title}
+          </Link>
+        ))}
+      </div>
     </div>
   );
 }
